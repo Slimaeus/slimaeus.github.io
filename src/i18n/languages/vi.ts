a687c7a7d7aea69b62fd4bd60e961c37a1ff3a96
Slimaeus <nguyenhongthai28042002@gmail.com>
2025-06-23 09:38:55 +0700

    Support only en and vi

---
+++ src/i18n/languages/vi.ts
@@ -0,0 +1,41 @@
+import Key from "../i18nKey";
+import type { Translation } from "../translation";
+
+export const vi: Translation = {
+	[Key.home]: "Trang chủ",
+	[Key.about]: "Tôi",
+	[Key.archive]: "Archive",
+	[Key.search]: "Tìm kiếm",
+	[Key.projects]: "Dự án",
+
+	[Key.tags]: "Nhãn",
+	[Key.categories]: "Danh mục",
+	[Key.recentPosts]: "Bài viết gần đây",
+
+	[Key.comments]: "Bình luận",
+
+	[Key.untitled]: "Không tiêu đề",
+	[Key.uncategorized]: "Không phân mục",
+	[Key.noTags]: "Không nhãn",
+
+	[Key.wordCount]: "từ",
+	[Key.wordsCount]: "từ",
+	[Key.minuteCount]: "phút",
+	[Key.minutesCount]: "phút",
+	[Key.postCount]: "bài viết",
+	[Key.postsCount]: "bài viết",
+	[Key.projectCount]: "dự án",
+	[Key.projectsCount]: "dự án",
+
+	[Key.themeColor]: "Màu chủ đề",
+
+	[Key.lightMode]: "Sáng",
+	[Key.darkMode]: "Tối",
+	[Key.systemMode]: "Hệ thống",
+
+	[Key.more]: "Thêm",
+
+	[Key.author]: "Tác giả",
+	[Key.publishedAt]: "Xuất bản lúc",
+	[Key.license]: "Giấy phép",
+};
